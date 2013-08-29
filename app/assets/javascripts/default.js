e353e4e1f88c9f39457cd4d7aa5f838a4ce06cb8
root <root@rmed0027lt-laptop.(none)>
2013-08-29 10:49:14 +0530

    first commit-heroku

---
+++ app/assets/javascripts/default.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmoblog::Application.config.secret_token = '87a7a3129726b7feee0e294a7f1c54919e790e4ab8a699f1cfd6fd03aa11061f1942f7dccf5652f483983b4eb5f7c0806c75166d66056a3412b9cd4ddafa15c4'
